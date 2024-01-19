41f7e37324e806089b81ab78da7cd9e5e3174c80
markkivoi <149739711+markkivoi@users.noreply.github.com>
2024-01-19 20:19:13 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/9e8c0285ac.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <h2 class="navbar-brand w-50 " href="#">H&H International Limited</h2>
+  <button class="navbar-toggler bg-primary" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+    <div class="navbar-nav text-primary">
+      <a class="nav-item nav-link active text-primary" href="#">HOME</a>
+      <a class="nav-item nav-link text-primary" href="#about">ABOUT</a>
+      <a class="nav-item nav-link text-primary" href="#service">SERVICES</a>
+      <a class="nav-item nav-link text-primary" href="#appointment">APPOINTMENT</a>
+      <a class="nav-item nav-link text-primary" href="#contact">CONTACT</a>
+    
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-5">
+    <div class="">
+    <br><br>
+       <p> Get INTERGRATED Healthcare <br>
+          For Your <br>
+          Healthy living</p>
+       <p class="text-justify">Experience exceptional care at our healthcare center, where your well-being is our priority.</p>
+       <button type="button" class="btn btn-primary m-3" id="btn-link"><a href="#">GET APPOINTMENT</a></button>
+    </div>
+</div>
+
+<div class="card-group h-50">
+  <div class="card m-3">
+    <img class="card-img-top" src="images/doctor3.jpeg" alt="Card image cap">
+  </div>
+  
+  <div class="card m-3">
+    <img class="card-img-top" src="images/doctor2.jpeg" alt="Card image cap">
+  </div>
+</div>
+
+<div class="card-group mt-3 h-50 w-50 ml-5">
+  <div class="card text-center">
+    <img class="card-img-top mx-auto" src="images/doctor.jpeg" alt="Card image cap">
+  </div>
+</div>
+
+<!--About-->
+<div class="ml-4 mt-5" id="about">
+   <h3 class="text-primary">ABOUT US</h3>
+   <h4> H&H (Health Happiness) International Ltd</h4>
+   <p class="text-justify">We are a unique healthcare company that provides INTERGRATED Healthcare 
+      (combined conventional medicine and alternative Therapy) globally.
+   </p>
+</div>
+
+<!--our services-->
+<div class=" mt-5" id="service">
+   <h3 class="text-primary ml-4">OUR SERVICES</h3>
+   <h4 class="ml-4"> H&H (Health Happiness) International Ltd</h4>
+   <p class="text-justify ml-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Aliquam culpa mollitia rem illo sequi dolore architecto doloribus, <br>
+    velit in exercitationem nobis veniam necessitatibus voluptatem quisquam non ipsam repellat odit quas?
+   </p>
+
+   <h5 class="text-primary ml-4">Service 1</h5>
+   <p class="text-justify ml-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Aliquam culpa mollitia rem illo sequi dolore architecto doloribus, <br>
+    velit in exercitationem nobis veniam necessitatibus voluptatem quisquam non ipsam repellat odit quas?
+   </p>
+
+   <h5 class="text-primary ml-4">Service 2</h5>
+   <p class="text-justify ml-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Aliquam culpa mollitia rem illo sequi dolore architecto doloribus, <br> 
+    velit in exercitationem nobis veniam necessitatibus voluptatem quisquam non ipsam repellat odit quas?
+   </p>
+   <h5 class="text-primary ml-4">Service 3</h5>
+   <p class="text-justify ml-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Aliquam culpa mollitia rem illo sequi dolore architecto doloribus, <br>
+    velit in exercitationem nobis veniam necessitatibus voluptatem quisquam non ipsam repellat odit quas?
+   </p>
+</div>
+
+<!--contact us-->
+<div id="appointment">
+<section class="mt-5">
+  <div class="">
+    <h2 class="text-center text-primary mb-3">Make An Appointment</h2>
+  </div>
+
+  <div class="w-75 m-auto">
+    <form action="" method="">
+
+               <div class="form-group">
+                 <label>Full Name:</label>
+                 <input type="text" name="name" class="form-control" autocomplete="off">
+               </div>
+         
+               <div class="form-group">
+                 <label>Email:</label>
+                 <input type="text" name="email" class="form-control" autocomplete="off">
+               </div>
+         
+               <div class="form-group">
+                 <label>Phone Number:</label>
+                 <input type="text" name="number" class="form-control" autocomplete="off">
+               </div>
+         
+               <div class="form-group">
+                 <label>Additional Information:</label>
+                 <textarea name="reason" id="" class="form-control" cols="30" rows="5"></textarea>
+               </div>
+         
+               <button type="Submit" class="btn btn-primary ml-4" > MAKE AN APPOINTMENT</button>
+    </form>
+
+  </div>
+</section>
+</div>
+
+<div class=" mt-5 bg-primary text-light" id="contact">
+  <div class=" ml-3">
+   <h4>CONTACT US</h4>
+  </div>
+  <div class="ml-2">
+   <h4> Get in Touch With us</h4>
+   <p class="text-light"> For INTERGRATED diagnosis, treatment and prevention of diseases and injuries in humans</p>
+  </div>
+  <div class="ml-2">
+  <i class="fa-solid fa-phone-volume bi fs-3"></i>
+  <h5>Call us</h5>
+  <p>+254 72734....<br>
+   +254 72534....</p>
+  </div>
+  <div class="ml-2">
+  <i class="fa-solid fa-envelope bi fs-3"></i>
+  <h5>Mail us 24/7</h5>
+  <p>hh.africahealth@gmail.com</p>
+  </div>
+  <div class="ml-2">
+  <i class="fa-solid fa-location-dot bi fs-3"></i>
+  </div>
+  <div class="ml-2">
+  <i class="fa-brands fa-square-facebook bi fs-3 "></i>
+  <i class="fa-brands fa-square-instagram bi fs-3"></i>
+  <i class="fa-brands fa-square-twitter bi fs-3 "></i>
+  <p>@oursocialmediapages</p>
+  </div>
+  
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
